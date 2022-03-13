22120c2f2ea19db89d08309a2b5bce550824fb2b
Aparche <15602205635@163.com>
2022-03-13 15:51:34 +0800

    config

---
+++ cloud-config-center1-3344/src/main/java/com/shijin/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.shijin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author shijin
+ * @create 2022-03-13 5:41 上午
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
